176013ddf9857a1dc5c30c19377674f52f887b1f
Roman-orn <0671635566@ukr.net>
2026-05-12 23:17:04 +0300

    Add functionality main class

--- src/app/DataHandler.java
+++ src/app/DataHandler.java
@@ -6,13 +6,13 @@ public class DataHandler {
 
     String[] fruits = new DataRepository().getData();
 
-    public void getOutput(){
+    public void getOutput() {
 
         synchronized (this) {
             StringBuilder sb = new StringBuilder();
             AtomicInteger count = new AtomicInteger(1);
-            for (String fruit : fruits){
-                sb.append(String.format("(%d) %s", count.getAndIncrement(), fruit));
+            for (String fruit : fruits) {
+                sb.append(String.format("(%d) %s ", count.getAndIncrement(), fruit));
             }
             System.out.println(Thread.currentThread().getName() + ": " + sb);
         }
--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,13 @@
 package app;
 
 public class Main {
+
+    static void main(String[] args) {
+
+        DataHandler dataHandler = new DataHandler();
+        MyThread myThread1 = new MyThread("Thread 1", dataHandler);
+        MyThread myThread2 = new MyThread("Thread 2", dataHandler);
+        myThread1.start();
+        myThread2.start();
+    }
 }
