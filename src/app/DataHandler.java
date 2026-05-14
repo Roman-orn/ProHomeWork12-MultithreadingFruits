0684bf6cf35280511baa729cd543b5eef78c2bea
Roman-orn <0671635566@ukr.net>
2026-05-14 22:35:48 +0300

    Fix variable count

--- src/app/DataHandler.java
+++ src/app/DataHandler.java
@@ -1,9 +1,10 @@
 package app;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class DataHandler {
 
     private final DataRepository dataRepository;
-    private int count;
 
     public DataHandler(DataRepository dataRepository) {
         this.dataRepository = dataRepository;
@@ -11,13 +12,12 @@ public class DataHandler {
 
     public void getOutput() {
 
-        synchronized (this) {
-            StringBuilder sb = new StringBuilder();
-            String[] fruits = dataRepository.getData();
-            for (String fruit : fruits) {
-                sb.append(String.format("(%d) %s ", ++count, fruit));
-            }
-            System.out.println(Thread.currentThread().getName() + ": " + sb);
+        StringBuilder sb = new StringBuilder();
+        String[] fruits = dataRepository.getData();
+        int count = 0;
+        for (String fruit : fruits) {
+            sb.append(String.format("(%d) %s ", ++count, fruit));
         }
+        System.out.println(Thread.currentThread().getName() + ": " + sb);
     }
 }
